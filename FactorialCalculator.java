dfba3643fb49c06a63bb574caf69c5d48bfb9702
christopher lopez <christopherwlopez89@gmail.com>
2023-10-28 14:05:43 -0700

    java factorial commit

---
+++ FactorialCalculator.java
@@ -0,0 +1,16 @@
+public class FactorialCalculator {
+
+    public static int calculateFactorial(int number){
+        if(number == 0 || number == 1){
+            return 1;
+        } else {
+            return number * calculateFactorial(number - 1);
+        }
+    }
+
+    public static void main(String[] args){
+        int number = 5;
+        int factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + "is: " + factorial);
+    }
+}
